1d85ad90ece3e471f4353953652485147354a4ef
Jimbo-beep <justice.tomlinson@mail.utoronto.ca>
2021-06-16 06:01:50 -0400

    First Update

--- app_rough/lib/main.dart
+++ app_rough/lib/main.dart
@@ -115,18 +115,43 @@ class TeamLists extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Your Team Lists"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-            // Navigate back to first route when tapped.
-          },
-          child: Text('Back'),
+        appBar: AppBar(
+          title: Text("Team ToDo Lists"),
         ),
-      ),
+        body: Center(
+          child: Container(
+            margin: const EdgeInsets.only(top: 545),
+            child: Row(
+              mainAxisAlignment:
+              MainAxisAlignment.spaceBetween,
+              children: [
+                new ButtonBar(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      child: Text('Back'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    SizedBox(width: 142),
+                    FloatingActionButton.extended(
+                      label: Text('Create A New List'),
+                      backgroundColor: Colors.orange,
+                      focusColor: Colors.white,
+                      onPressed: () {
+
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => YourLists()));
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
     );
   }
 }
@@ -154,51 +179,86 @@ class Settings extends StatelessWidget {
 
 
 class ToDoManagement extends StatefulWidget {
-  @override
 
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  final List tasks;
+  ToDoManagement({required this.tasks});
+  @override
+  _ToDoManagementState createState() => _ToDoManagementState();
   }
-  List _allTasks = [];
 
+class _ToDoManagementState extends State<ToDoManagement> {
+  Map toDoLists = {};
+  addList(){
+    var newList;
+    toDoLists[newList] = [];
 
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  bool isChecked = false;
-
+  }
   @override
   Widget build(BuildContext context) {
-    Color getColor(Set<MaterialState> states) {
-      const Set<MaterialState> interactiveStates = <MaterialState>{
-        MaterialState.pressed,
-        MaterialState.hovered,
-        MaterialState.focused,
-      };
-      if (states.any(interactiveStates.contains)) {
-        return Colors.blue;
-      }
-      return Colors.red;
-    }
-
-    return Checkbox(
-      checkColor: Colors.white,
-      fillColor: MaterialStateProperty.resolveWith(getColor),
-      value: isChecked,
-      onChanged: (bool? value) {
+    return GestureDetector(
+      onTap: () {
         setState(() {
-          isChecked = value!;
+          for( var i = 0 ; i < toDoLists.length; i++) {
+
+                          }
+          // I want to create a new button for every list in the list
+          // list;
         });
       },
     );
   }
 }
+
+class ToDoList extends StatefulWidget {
+  final String task;
+  const ToDoList({required this.task});
+  @override
+  _ToDoListState createState() => _ToDoListState();
+}
+
+
+class _ToDoListState extends State<ToDoList> {
+  List allItems = [];
+  @override
+  Widget build(BuildContext context) {
+    return Text('allItems');
+  }
+}
+/// This is the private State class that goes with MyStatefulWidget. ALSO MAY WANT TO USE THE BELOW FOR CHeckboxes
+// class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+//   bool isChecked = false;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     Color getColor(Set<MaterialState> states) {
+//       const Set<MaterialState> interactiveStates = <MaterialState>{
+//         MaterialState.pressed,
+//         MaterialState.hovered,
+//         MaterialState.focused,
+//       };
+//       if (states.any(interactiveStates.contains)) {
+//         return Colors.blue;
+//       }
+//       return Colors.red;
+//     }
+//
+//     return Checkbox(
+//       checkColor: Colors.white,
+//       fillColor: MaterialStateProperty.resolveWith(getColor),
+//       value: isChecked,
+//       onChanged: (bool? value) {
+//         setState(() {
+//           isChecked = value!;
+//         });
+//       },
+//     );
+//   }
+// }
+
+
+// class MyStatefulWidget extends StatefulWidget {
+//   const MyStatefulWidget({Key? key}) : super(key: key);
+//
+//   @override
+//   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+// }
